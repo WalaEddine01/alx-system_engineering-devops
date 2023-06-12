78a698494659adb39a45520733b5f4e2364d0619
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-12 23:44:27 +0000

    wala eddine

--- 0x02-shell_redirections/oto.sh
+++ 0x02-shell_redirections/oto.sh
@@ -2,6 +2,6 @@
 # Add all files to the staging area 
 git add .  
 # Commit changes with a specified message
-git commit -m "Automated commit"  
+git commit -m "wala eddine"  
 # Push changes to the remote repository 
 git push
