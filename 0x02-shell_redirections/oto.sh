1f929b0db2f273d28c18dffd7189bdfcca072a76
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-12 22:11:20 +0000

    Automated commit

---
+++ 0x02-shell_redirections/oto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash   
+# Add all files to the staging area 
+git add .  
+# Commit changes with a specified message
+git commit -m "Automated commit"  
+# Push changes to the remote repository 
+git push
