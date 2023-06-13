9374856ff0ec922d972aac650e1775c8db170ca4
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-13 20:10:51 +0000

    wala eddine

---
+++ 0x03-shell_variables_expansions/oto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Add all files to the staging area 
+git add . 
+# Commit changes with a specified message 
+git commit -m "wala eddine" 
+# Push changes to the remote repository 
+git push
