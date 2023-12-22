9166ff32907fdeed524f118a35a14d1ef245a003
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-22 22:44:55 +0100

    I made the changes on 2023-12-22 22:44:55

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating file in /tmp. Using Puppet
+file { '/tmp/school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
