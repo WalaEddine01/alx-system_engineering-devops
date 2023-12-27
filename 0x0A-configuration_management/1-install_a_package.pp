974922ac45054a09b9a280ac07bce53db1078649
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-27 16:46:52 +0100

    I made the changes on 2023-12-27 16:46:51

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,23 @@
 # installing flask from pip3
-package {
-  'flask':
-  ensure   => 'present',
-  provider => 'pip3',
+# Ensure Python 3.8.10
+package { 'python3':
+ensure   => '3.8.10',
+provider => 'apt',
+}
+# Ensure pip3
+package { 'python3-pip':
+ensure  => 'present',
+require => Package['python3'],
+}
+# Installing Werkzeug
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
+require  => Package['python3-pip']
+}
+# Installing flask
+package { 'Flask':
+ensure   => '2.1.0',
+provider => 'pip3',
+require  => Package['python3-pip'],
 }
