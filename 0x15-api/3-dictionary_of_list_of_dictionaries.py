51e66c92e05f818c07b6138b4920989f6a2ff116
walaeddine01 <walaaeddine33@gmail.com>
2024-01-31 17:28:34 +0100

    I made the changes on 2024-01-31 17:28:34

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
             res = json.load(request_)
             for i in res:
                 ID_ = i.get('id')
-                print('1--------')
                 username = i.get('username')
                 url2 = "https://jsonplaceholder.typicode.com/todos"
                 with request.urlopen(url2) as req:
@@ -24,19 +23,15 @@ if __name__ == "__main__":
                     json_data_list[str(ID_)] = []
                     for j in range(len(res2)):
                         dict_ = {}
-                        print('2--------')
                         if res2[j].get("userId") is int(ID_):
                             completed = res2[j].get('completed')
                             title = res2[j].get("title")
                             dict_['task'] = title
                             dict_['completed'] = completed
                             dict_['username'] = username
-                            print('3--------')
                             json_data_list[str(ID_)].append(dict_)
-                            print('4--------')
             json_file_path = "todo_all_employees.json"
             jsonfile = open(json_file_path, 'w', newline='')
-            print('4--------')
             json.dump(json_data_list, jsonfile)
             jsonfile.close()
     except Exception as e:
