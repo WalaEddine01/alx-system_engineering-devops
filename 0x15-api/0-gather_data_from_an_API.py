561dc1840e8d04b8ff7e2d61d194da48047195d6
walaeddine01 <walaaeddine33@gmail.com>
2024-01-31 13:56:15 +0100

    I made the changes on 2024-01-31 13:56:15

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+"""
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
+    import json
+    ID_ = argv[1]
+    list_ = []
+    b = 0
+    url = "https://jsonplaceholder.typicode.com/users"
+    try:
+        with request.urlopen(url) as request_:
+            res = json.load(request_)
+            for i in res:
+                if i.get('id') == int(ID_):
+                    name = i.get('name')
+                    break
+            url2 = "https://jsonplaceholder.typicode.com/todos"
+            with request.urlopen(url2) as req:
+                res2 = json.load(req)
+                a = 0
+                for i in res2:
+                    if i.get("userId") is int(ID_)\
+                            and i.get("completed") is True:
+                        a = a + 1
+                        list_.append(i.get("title"))
+                    if i.get("userId") == int(ID_):
+                        b = b + 1
+                print(f"Employee {name} is done with tasks({a}/{b}):")
+                for i in list_:
+                    print("\t ", end="")
+                    print(i)
+    except Exception as e:
+        print(f"Error code: {e}")
