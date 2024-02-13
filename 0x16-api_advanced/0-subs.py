12304e1ee7438b9b8acec55135aa74f63e354fa1
walaeddine01 <walaaeddine33@gmail.com>
2024-02-13 16:44:49 +0100

    I made the changes on 2024-02-13 16:44:49

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-this module contains a function that queries the Reddit API and returns
-the number of subscribers (not active users, total subscribers) for a given
+0x16. API advanced
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API
-    and returns the number of subscribers
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {
-        "User-Agent": "custom "
-    }
-    res = requests.get(url, headers=header, allow_redirects=False)
-    if res.status_code == 200:
-        if res.json().get('data'):
-            return res.json()["data"]["subscribers"]
+    """A function that returns the number of subscribers
+    to an account if existed."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(
+        url,
+        headers={"User-Agent": "custom"},
+    )
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    else:
         return 0
-    return 0
