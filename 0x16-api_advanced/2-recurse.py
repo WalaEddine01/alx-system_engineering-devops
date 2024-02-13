3d63174d883da86c93796774229805d5bc45dd84
walaeddine01 <walaaeddine33@gmail.com>
2024-02-13 22:51:03 +0100

    I made the changes on 2024-02-13 22:51:03

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this module contains a function that queries
+the Reddit API and returns the number of subscribers
+"""
+from requests import get
+
+
+header = {
+    "User-Agent": "Mozilla/5.0"
+    }
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    param = {'after': 1} if param else {}
+    res = get(url=url, headers=header, allow_redirects=False, params=param)
+    if res.status_code == 200:
+        for post in res.json().get('data', {}).get('children', []):
+            hot_list.append(post.get('data', {}).get('title'))
+        if res.json().get('data', {}).get('after'):
+            return recurse(subreddit, hot_list,
+                           after=res.json().get('data', {}).get('after'))
+        return hot_list
+    return None
