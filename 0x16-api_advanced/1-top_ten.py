e9a80fc079b85b338ca239ada549c441d512469d
walaeddine01 <walaaeddine33@gmail.com>
2024-02-13 17:47:41 +0100

    I made the changes on 2024-02-13 17:47:41

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,18 @@ the Reddit API and returns the number of subscribers
 
 from requests import get
 
+
 def top_ten(subreddit):
     """
+    function that queries the Reddit API
+    and returns the number of subscribers
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     header = {
         "User-Agent": "Mozilla/5.0"
-    }
+        }
+    res = get(url=url, headers=header, allow_redirects=False)
+    if res.status_code == 200:
+        for post in res.json().get('data', {}).get('children', []):
+            print(post['data']['title'])
+    print(None)
