9d928f54417f148b832e31d88bea87f057f95883
walaeddine01 <walaaeddine33@gmail.com>
2024-02-13 16:29:11 +0100

    I made the changes on 2024-02-13 16:29:11

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+this module contains a function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers) for a given
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API
+    and returns the number of subscribers   
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {
+        "User-Agent": "Mozilla"
+    }
+    res = requests.get(url, headers=header, allow_redirects=False)
+    if res.status_code == 200:
+        if res.json().get('data'):
+            return res.json().get('data').get('subscribers')
+        return 0
+    return 0
