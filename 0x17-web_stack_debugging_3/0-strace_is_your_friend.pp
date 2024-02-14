e73fe8081f00869f3d60d1045a8239e83c984403
walaeddine01 <walaaeddine33@gmail.com>
2024-02-14 20:52:15 +0100

    I made the changes on 2024-02-14 20:52:15

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
+# This is a simple example of how to use the exec resource to run a command on the system.
 $path_file = '/var/www/html/wp-settings.php'
 
-exec { 'replace_string':
+exec { 'fix-wordpress':
   command => "sed -i 's/phpp/php/g' ${path_file}", # replace phpp with php
   path    => ['/usr/bin', '/bin', '/usr'] # or wherever sed is located
 }
