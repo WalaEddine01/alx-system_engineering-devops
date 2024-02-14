0efffc52f1a12f58ad5c4cfb5db08a86a4763d56
walaeddine01 <walaaeddine33@gmail.com>
2024-02-14 20:44:49 +0100

    I made the changes on 2024-02-14 20:44:49

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 $path_file = '/var/www/html/wp-settings.php'
 
 exec { 'replace_string':
-  command => " -i 's/phpp/php/g' ${path_file}", # replace phpp with php
+  command => "sed -i 's/phpp/php/g' ${path_file}", # replace phpp with php
   path    => ['/usr/bin', '/bin', '/usr'] # or wherever sed is located
 }
