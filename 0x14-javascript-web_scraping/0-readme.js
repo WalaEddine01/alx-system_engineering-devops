e893340c31345fb2bc986c8fce7fa1a4df9f37a5
walaeddine01 <walaaeddine33@gmail.com>
2024-02-18 17:50:23 +0100

    I made the changes on 2024-02-18 17:50:22

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const fs = require('fs');
-const { argv } = require('process');
-
-console.log(fs.readFileSync(argv[2], 'utf-8'));
--- 0x14-javascript-web_scraping/wal.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-
-request('http://google.com/doodle.png').pipe(fs.createWriteStream('doodle.png'))
