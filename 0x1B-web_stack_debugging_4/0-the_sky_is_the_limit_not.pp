9d32e92e2484bec403313eb47e2a84b1adc8fb4c
walaeddine01 <walaaeddine33@gmail.com>
2024-02-22 15:51:20 +0100

    I made the changes on 2024-02-22 15:51:20

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env puppet
+# this is a puppet script to fix the issue with the number of arguments
+
+$path2 = '/var/www/html'
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+exec { 'fix_server':
+  command => "/bin/sed -i 's|root /usr/share/nginx/html;|root ${path2};|' /etc/nginx/sites-available/default",
+  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+  onlyif  => "/bin/grep -q 'root /usr/share/nginx/html;' /etc/nginx/sites-available/default",
+  notify  => Service['nginx'],
+}
+
